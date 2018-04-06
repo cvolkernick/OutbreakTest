db64e8f0bc545c53eb3fe1ea742bf88b1f8c2127
Chris Volkernick <cvolkern@gmail.com>
2018-04-06 12:34:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/admin/outbreaktest/model/Room.java
@@ -0,0 +1,143 @@
+package com.example.admin.outbreaktest.model;
+
+public class Room {
+    public final boolean isInfected;
+    public boolean visited = false;
+
+    public Room(boolean infected) {
+        isInfected = infected;
+    }
+
+    public static boolean isOutbreak(Room[][] floor) {
+        for (int i = 0; i < floor.length; i++) {
+            for (int j = 0; j < floor[i].length; j++) {
+                if( infectedNeighbor(floor,i,j)>4 )
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public static int infectedNeighbor(Room[][] floor,int x,int y){
+        if( x >= 0  && x < floor.length && y >= 0 && y < floor[0].length){
+            if (!floor[x][y].visited && floor[x][y].isInfected){
+                floor[x][y].visited = true;
+                return 1+infectedNeighbor(floor,x,y-1)
+                        +infectedNeighbor(floor,x+1,y)
+                        +infectedNeighbor(floor,x-1,y)
+                        +infectedNeighbor(floor,x,y+1);
+            }else{
+                floor[x][y].visited = true;
+                return 0;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Room[][] verticalTrue = new Room[][] {
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(true), new
+                        Room(true), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) }
+        };
+
+        Room[][] horizontalTrue = new Room[][] {
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(true), new Room(true), new
+                        Room(true), new Room(true), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) }
+        };
+
+        Room[][] noInfection = new Room[][] {
+                {new Room(true), new Room(false), new Room(true), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(true), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(true), new Room(false), new Room(true), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(true), new
+                        Room(false), new Room(true), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(true), new Room(false), new Room(false), new
+                        Room(true), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(true), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(true), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) },
+                {new Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false), new Room(false), new Room(false), new Room(false), new
+                        Room(false) }
+        };
+
+        System.out.println("noInfection : " + isOutbreak(noInfection));
+        System.out.println("horizontalTrue : " + isOutbreak(horizontalTrue));
+        System.out.println("verticalTrue : " + isOutbreak(verticalTrue));
+    }
+
+}
